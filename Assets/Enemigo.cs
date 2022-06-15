61621ae60c207950388df422782c351e30d33ca6
JuanAOrt22 <101871735+JuanAOrt22@users.noreply.github.com>
2022-06-15 14:28:50 -0300

    NavMeshhh

--- Assets/Enemigo.cs
+++ Assets/Enemigo.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Enemigo : MonoBehaviour
 {
 
     public int vida;
-    // Start is called before the first frame update
+    public float distanciadever = 10f;
+
+    public Transform target;
+    NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
     public void TakeDamage(int damage)
     {
         vida -= damage;
@@ -16,9 +23,26 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= distanciadever)
+        {
+            agent.SetDestination(target.position);
+        }
+        if(distance <= agent.stoppingDistance)
+        {
+            GirarAlJugador();
+        }
         if(vida <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    void GirarAlJugador()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion Rotacionmirar = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, Rotacionmirar, Time.deltaTime * 5f);
     }
 }
